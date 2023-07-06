c43c14fb3d0e0b2f50c93589e3cd87911f387d01
Quang Dang <qdang@umass.edu>
2023-07-07 01:20:30 +0700

    Made changes to styling (mainly fonts) and moved stuff around

--- components/Discover.jsx
+++ components/Discover.jsx
@@ -3,26 +3,26 @@ import '@styles/discovery.css';
 
 export const Discovery = () => {
     return (
-        <div class="discovery">
+        <div className="discovery">
             <div id="header-disc"><h1>Welcome User!</h1></div>
-            <div class="container">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Course List</h5>
-                        <p class="card-text">View information regarding any CICS course and add it to your schedule</p>
+            <div className="container">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Course List</h5>
+                        <p className="card-text">View information regarding any CICS course and add it to your schedule</p>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Profile</h5>
-                        <p class="card-text">Personalize your own profile and edit your plan</p>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Profile</h5>
+                        <p className="card-text">Personalize your own profile and edit your plan</p>
                     </div>
                 </div>
 
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Course Graph</h5>
-                        <p class="card-text">View all of CICS's current offering with an interactive graph</p>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Course Graph</h5>
+                        <p className="card-text">View all of CICS&apos;s current offering with an interactive graph</p>
                     </div>
                 </div>
             </div>
